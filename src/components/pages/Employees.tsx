bfabf2e57a63040619f67a4048a08277b44cc39a
ilya kruglya <bcrash1@gmail.com>
2023-02-19 18:07:23 +0200

    HW #42 Statistics DataGrid

--- src/components/pages/Employees.tsx
+++ src/components/pages/Employees.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {Box, List, ListItem, Typography} from '@mui/material';
 import { useSelector } from 'react-redux';
 import { Employee } from '../../model/Employee';
+import { DataGrid, GridColumns } from '@mui/x-data-grid';
+import './table.css';
 export const Employees: React.FC = () => {
+    const columns = React.useRef<GridColumns>([
+        {field: 'name', headerName: 'Employee Name', flex: 1, headerAlign: 'center', align: 'center', headerClassName: 'header'}, 
+        {field: 'birthDate', headerName: 'Date of Birth', flex: 1, type: 'date', headerAlign: 'center', align: 'center', headerClassName: 'header'},
+        {field: 'department', headerName: 'Department', flex: 1, headerAlign: 'center', align: 'center', headerClassName: 'header'},
+        {field: 'salary', headerName: 'Salary (NIS)', flex: 0.8, type: 'number', headerAlign: 'center', align: 'center', headerClassName: 'header'}
+    ])
     const employees = useSelector<any, Employee[]>(state => state.employees.employees);
-    return <Box>
-        <List>
-            {getListItems(employees)}
-        </List>
+    return <Box sx = {{height: "80vh", width: "80vw"}}>
+        <DataGrid columns={columns.current} rows={employees}/>
     </Box>
 }
 function getListItems(employees: Employee[]): React.ReactNode {
---
+++ src/model/Statistic.ts
@@ -0,0 +1,8 @@
+export type Statistics = {
+    id: number;
+    birthDate: number;
+    salary: number;
+    minValue: number;
+    maxValue: number;
+    avarageValue: number;
+}
